4717c060241f52b4c2a7d7de556662a722de8364
Ihor Niemyi <igor.niemyi.ios.developer@gmail.com>
2024-11-21 20:40:57 +0200

    UI and file structure changes

--- Cinemania/AppDelegate.swift
+++ Cinemania/AppDelegate.swift
@@ -7,12 +7,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-        
         return true
     }
     
     // MARK: UISceneSession Lifecycle
-    
     func application(
         _ application: UIApplication,
         configurationForConnecting connectingSceneSession: UISceneSession,
